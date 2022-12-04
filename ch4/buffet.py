44f10611551576bd819a081b573bfed0197402fb
rtyner <rusty@rtyner.com>
2022-12-04 14:12:32 -0500

    completed ch4

---
+++ ch4/buffet.py
@@ -0,0 +1,9 @@
+foods = ('steak', 'chicken', 'fish', 'potatoes', 'cake')
+print("Foods availabile:")
+for food in foods:
+    print(food)
+
+foods = ('steak', 'chicken', 'fish', 'vegetables', 'pie')
+print("New foods availabile:")
+for food in foods:
+    print(food)
