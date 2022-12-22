103434e1bb6904f2fc4f5b7c379eacae705c2bea
rt <rusty@rtyner.com>
2022-12-22 01:50:58 +0000

    continuing ch

---
+++ ch8/messages.py
@@ -0,0 +1,7 @@
+def show_messages(messages):
+    for message in messages:
+        msg = f"{messages}"
+        print(msg)
+
+messages = ['bingo hello', 'i need to sleep', 'i dont want to work tomorrow']
+show_messages(messages)
