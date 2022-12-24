d8fa644db87981e795bd6331c4a1b4167e347961
rtyner <rusty@rtyner.com>
2022-12-24 18:10:01 -0500

    added comments

---
+++ ch8/sending_messages.py
@@ -0,0 +1,8 @@
+def send_messages(messages):
+    """prints messages from a list"""
+    for message in messages:
+        printed = f"{message}"
+    
+
+messages = ['bingo hello', 'i need to sleep', 'i dont want to work tomorrow']
+
